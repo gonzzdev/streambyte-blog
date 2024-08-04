4eccf28a9f41b6b37840cc574029a19288f16f06
Gonzz <streambytemx@gmail.com>
2024-08-03 17:50:19 -0700

    Actualización de home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import animations from '@midudev/tailwind-animations'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
@@ -21,5 +24,5 @@ export default {
 			}
 		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
